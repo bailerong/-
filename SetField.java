ef3d58db22ca971dee9212249177f2d522a744c2
bailerong <15929276532@163.com>
2019-03-30 22:49:08 +0800

    这是我们的通过反射设置类属性值的方法

--- SetField.java
+++ SetField.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Field;
 
+/*
+* 这是一个我们通过反射设置属性的代码
+*
+* */
+class Person1{
+
+    public String name;
+}
 public class SetField {
+    public static void main(String[] args) throws Exception {
+        Class<?> cls=Class.forName("Person1");
+        Object object=cls.newInstance();
+        //实例化操作我们的类属性
+        Field nameField=cls.getDeclaredField("name");
+        nameField.set(object,"lele");
+        System.out.println(nameField.get(object));
+    }
 }
