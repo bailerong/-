6289abca80b51f48b0d2c1c3292fcdf273cde471
bailerong <15929276532@163.com>
2019-03-30 22:50:30 +0800

    这是我们通过反射破坏类属性封装的代码

--- rudeAccess.java
+++ rudeAccess.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.lang.reflect.Field;
 
+/*
+* 这是我们的一个破坏类中属性封装的代码
+*/
+class Person2{
+    private String name;
+}
 public class rudeAccess {
+    public static void main(String[] args) throws Exception {
+        Class<?> cls=Class.forName("Person2");
+        Object object=cls.newInstance();
+        Field field=cls.getDeclaredField("name");
+        field.setAccessible(true);
+        field.set(object,"lele");
+        System.out.println(field.get(object));
+    }
 }
